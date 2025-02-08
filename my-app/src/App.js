e74a1f77bc62fba42a3f5131331922365f4fdd1e
Sai Teja Rachamalla <87630632+saitejar110507@users.noreply.github.com>
2025-02-08 14:17:58 +0000

    Added form component

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", backgroundColor: "#f5f5f5" }}>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/reportWebVitals.js
@@ -0,0 +1,9 @@
+import { reportWebVitals as webVitals } from 'web-vitals';
+
+// Your custom logic (if any)
+const reportWebVitals = (metric) => {
+  console.log(metric);
+};
+
+// Export with named export
+export { reportWebVitals };
